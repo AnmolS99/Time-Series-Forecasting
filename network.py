364917711d8c189b2ce8513e209e80a6e5119e25
Anmol Singh <Anmol_singh@live.no>
2022-04-25 21:09:12 +0200

    Fixed plotting and added one-hot

--- network.py
+++ network.py
@@ -2,6 +2,7 @@ import math
 from matplotlib import pyplot as plt
 import numpy as np
 import tensorflow as tf
+import random
 
 from preprocessing import Preprocessor
 
@@ -30,21 +31,35 @@ class RNN:
                     train_X,
                     train_Y,
                     validation_data,
-                    epochs=3,
+                    epochs=10,
                     batch_size=32):
 
-        self.model.fit(train_X,
-                       train_Y,
-                       validation_data=validation_data,
-                       epochs=epochs,
-                       batch_size=batch_size)
-        self.model.save(
-            f"models/model_seq{self.seq_len}_epochs{epochs}_batch{batch_size}")
+        # Creating model name
+        model_name = f"model_seq{self.seq_len}_epochs{epochs}_batch{batch_size}"
+
+        # Maybe try fitting  with shuffle=True
+        history = self.model.fit(train_X,
+                                 train_Y,
+                                 validation_data=validation_data,
+                                 epochs=epochs,
+                                 batch_size=batch_size)
+
+        # Plotting and saving the loss
+        plt.plot(history.history["loss"])
+        plt.plot(history.history["val_loss"])
+        plt.title("Model loss")
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['loss', 'val_loss'], loc='upper right')
+        plt.savefig(f"losses/{model_name}")
+        plt.show()
+
+        self.model.save(f"models/{model_name}")
 
     def load_model(self, model_path):
         self.model = tf.keras.models.load_model(model_path)
 
-    def predict_ahead(self, x, y, start, steps_ahead):
+    def predict_ahead(self, x, y, start, steps_ahead, show_graphs=False):
         """
         NB! Assumes that y_prev is the last column
         """
@@ -52,6 +67,7 @@ class RNN:
 
         # First time predicting
         seq = x[[start]]
+        history = seq[0, :, -1]
         pred = self.model.predict(seq)
         preds.append(pred.flatten())
         for i in range(start + 1, start + steps_ahead):
@@ -66,10 +82,13 @@ class RNN:
 
             preds.append(pred.flatten())
 
-        history = y[start:start + self.seq_len]
-        target = y[start + self.seq_len - 1:start + self.seq_len + steps_ahead]
         last_history = float(history[-1])
+        target = y[start:start + steps_ahead]
+        target = [last_history] + list(target)
+        target = np.array(target, dtype=object)
+
         preds = [last_history] + preds
+        preds = np.array(preds, dtype=object)
 
         x_history = np.arange(start, start + self.seq_len)
         x_target = np.arange(start + self.seq_len - 1,
@@ -77,17 +96,41 @@ class RNN:
         x_preds = np.arange(start + self.seq_len - 1,
                             start + self.seq_len + steps_ahead)
 
+        if show_graphs:
+            plt.plot(x_history, history, label="history")
+            plt.plot(x_target, target, label="target")
+            plt.plot(x_preds, preds, label="predictions")
+            plt.legend()
+            plt.show()
+
         return x_history, history, x_target, target, x_preds, preds
 
-    def predict_multiple_series(self, x, y, start, steps_ahead, num_series):
+    def predict_multiple_series(self,
+                                x,
+                                y,
+                                start,
+                                steps_ahead,
+                                num_series,
+                                random_series=True):
         """
         NB! Assumes that y_prev is the last column
         """
 
         fig, ax = plt.subplots(nrows=2, ncols=math.ceil(num_series / 2))
+
+        if random_series:
+            x_length = len(x)
+            series_list = [
+                random.randint(0, x_length - steps_ahead - 1)
+                for _ in range(num_series)
+            ]
+        else:
+            series_list = [
+                start + (i * steps_ahead) for i in range(num_series)
+            ]
         for i in range(num_series):
             x_history, history, x_target, target, x_preds, preds = self.predict_ahead(
-                x, y, start + (i * steps_ahead), steps_ahead)
+                x, y, series_list[i], steps_ahead)
             row = i // math.ceil(num_series / 2)
             col = i % math.ceil(num_series / 2)
             ax[row, col].plot(x_history, history)
@@ -95,7 +138,6 @@ class RNN:
             ax[row, col].plot(x_preds, preds)
             ax[row, col].set_title(f"Series {i}")
 
-        # ax[0, math.ceil(num_series / 2) - 1].legend()
         fig.legend(["history", "target", "predictions"], loc="upper right")
 
         plt.show()
@@ -104,39 +146,39 @@ class RNN:
 if __name__ == "__main__":
     pp = Preprocessor(train_path="datasets/no1_train.csv",
                       val_path="datasets/no1_validation.csv")
-    train_df, val_df = pp.preprocess()
-    seq_len = 144
-    X_feat = [
-        "hydro", "micro", "thermal", "wind", "river", "total", "sys_reg",
-        "flow", "time_of_hour_sin", "time_of_hour_cos", "time_of_day_sin",
-        "time_of_day_cos", "time_of_week_sin", "time_of_week_cos",
-        "time_of_year_sin", "time_of_year_cos", "y_24h", "y_prev"
-    ]
-    num_feat = len(X_feat)
-    rnn = RNN(seq_len=seq_len, num_feat=num_feat)
+    train_df, val_df, X_feat = pp.preprocess()
+    seq_len = 72
+    rnn = RNN(seq_len=seq_len, num_feat=len(X_feat))
 
     train_X = pp.df_to_x(train_df[X_feat], seq_len=seq_len)
     train_Y = pp.df_to_y(train_df["y"], seq_len=seq_len)
     val_X = pp.df_to_x(val_df[X_feat], seq_len=seq_len)
     val_Y = pp.df_to_y(val_df["y"], seq_len=seq_len)
 
-    # Maybe change batch_size to make training go faster
-    rnn.train_model(train_X,
-                    train_Y,
-                    validation_data=(val_X, val_Y),
-                    epochs=10,
-                    batch_size=32)
+    # rnn.train_model(train_X,
+    #                 train_Y,
+    #                 validation_data=(val_X, val_Y),
+    #                 epochs=7,
+    #                 batch_size=32)
 
-    rnn.load_model("models/model_seq72_epochs1_batch32")
+    rnn.load_model(f"models/model_seq{seq_len}_epochs7_batch32")
+
+    # rnn.predict_ahead(x=val_X,
+    #                   y=val_Y,
+    #                   start=0,
+    #                   steps_ahead=1,
+    #                   show_graphs=True)
 
     # [
-    #     [y_prev1, y_prev2, y_prev3, y_prev4, y_prev5] 'y5 aka y_prev6'
-    #     [t2, t3, t4, t5, 't6'] 't7'
-    #     [t3, t4, t5, 't6', 't7'] 't8'
+    #     [y_prev1, y_prev2, y_prev3, y_prev4, y_prev5] 'y_prev6'
+    #     [y_prev2, y_prev3, y_prev4, y_prev5, 'y_prev6'] 'y_prev7'
+    #     [y_prev3, y_prev4, y_prev5, 'y_prev6', 'y_prev7'] 'y_prev8'
+    #     [y_prev4, y_prev5, 'y_prev6', 'y_prev7', 'random_noise'] 'y_prev9'
     # ]
 
     rnn.predict_multiple_series(x=val_X,
                                 y=val_Y,
                                 start=0,
                                 steps_ahead=24,
-                                num_series=6)
+                                num_series=6,
+                                random_series=True)
--- preprocessing.py
+++ preprocessing.py
@@ -24,42 +24,95 @@ class Preprocessor:
         Adding time of hour (minutes)
         """
         date_time = pd.to_datetime(df["start_time"])
-        df["time_of_hour_sin"] = np.sin(date_time.dt.minute *
-                                        ((2 * np.pi) / 60))
-        df["time_of_hour_cos"] = np.cos(date_time.dt.minute *
-                                        ((2 * np.pi) / 60))
-        return df
+        df["min_hour_sin"] = np.sin(date_time.dt.minute * ((2 * np.pi) / 60))
+        df["min_hour_cos"] = np.cos(date_time.dt.minute * ((2 * np.pi) / 60))
+        return df, ["min_hour_sin", "min_hour_cos"]
 
-    def add_time_of_day(self, df):
+    def add_time_of_day_min(self, df):
         """
-        Adding time of day (hours)
+        Adding time of day (minute)
         """
         date_time = pd.to_datetime(df["start_time"])
-        df["time_of_day_sin"] = np.sin(date_time.dt.hour * ((2 * np.pi) / 24))
-        df["time_of_day_cos"] = np.cos(date_time.dt.hour * ((2 * np.pi) / 24))
-        return df
+        min_in_day = 60 * 24
+        df["min_day_sin"] = np.sin(
+            ((date_time.dt.hour * 60) + date_time.dt.minute) *
+            ((2 * np.pi) / min_in_day))
+        df["min_day_cos"] = np.cos(
+            ((date_time.dt.hour * 60) + date_time.dt.minute) *
+            ((2 * np.pi) / min_in_day))
+        return df, ["min_day_sin", "min_day_sin"]
 
-    def add_time_of_week(self, df):
+    def add_time_of_day_hour(self, df):
+        """
+        Adding time of day (hour)
+        """
+        date_time = pd.to_datetime(df["start_time"])
+        df["hour_day_sin"] = np.sin(date_time.dt.hour * ((2 * np.pi) / 24))
+        df["hour_day_cos"] = np.cos(date_time.dt.hour * ((2 * np.pi) / 24))
+        return df, ["hour_day_sin", "hour_day_cos"]
+
+    def add_time_of_week_sin_cos(self, df):
         """
         Adding time of week (days)
         """
         date_time = pd.to_datetime(df["start_time"])
-        df["time_of_week_sin"] = np.sin(date_time.dt.day_of_week *
-                                        ((2 * np.pi) / 7))
-        df["time_of_week_cos"] = np.cos(date_time.dt.day_of_week *
-                                        ((2 * np.pi) / 7))
-        return df
+        df["day_week_sin"] = np.sin(date_time.dt.day_of_week *
+                                    ((2 * np.pi) / 7))
+        df["day_week_cos"] = np.cos(date_time.dt.day_of_week *
+                                    ((2 * np.pi) / 7))
+        return df, ["day_week_sin", "day_week_cos"]
 
-    def add_time_of_year(self, df):
+    def add_time_of_week_one_hot(self, df):
+        """
+        Adding time of week (days)
+        """
+        date_time = pd.to_datetime(df["start_time"])
+        df["day_week"] = "day_" + date_time.dt.day_of_week.astype(str)
+
+        possible_categories = [f"day_{i}" for i in range(7)]
+        # Getting one-hot encoding of month_year
+        one_hot_day = pd.get_dummies(df.day_week, prefix="", prefix_sep="")
+        one_hot_day = one_hot_day.reindex(columns=possible_categories,
+                                          fill_value=0)
+
+        # List of the feature names
+        feats = list(one_hot_day.columns)
+        # Adding one-hot columns
+        df = df.join(one_hot_day)
+        # Dropping month_year
+        df = df.drop(columns=["day_week"])
+        return df, feats
+
+    def add_time_of_year_sin_cos(self, df):
         """
         Adding time of year (month)
         """
         date_time = pd.to_datetime(df["start_time"])
-        df["time_of_year_sin"] = np.sin(date_time.dt.month *
-                                        ((2 * np.pi) / 12))
-        df["time_of_year_cos"] = np.cos(date_time.dt.month *
-                                        ((2 * np.pi) / 12))
-        return df
+        df["month_year_sin"] = np.sin(date_time.dt.month * ((2 * np.pi) / 12))
+        df["month_year_cos"] = np.cos(date_time.dt.month * ((2 * np.pi) / 12))
+        return df, ["month_year_sin", "month_year_cos"]
+
+    def add_time_of_year_one_hot(self, df):
+        """
+        Adding time of year (months)
+        """
+        date_time = pd.to_datetime(df["start_time"])
+        df["month_year"] = "month_" + date_time.dt.month.astype(str)
+
+        possible_categories = [f"month_{i}" for i in range(1, 13)]
+        # Getting one-hot encoding of month_year
+        one_hot_month = pd.get_dummies(df.month_year, prefix="", prefix_sep="")
+        one_hot_month = one_hot_month.reindex(columns=possible_categories,
+                                              fill_value=0)
+
+        # List of the feature names
+        feats = list(one_hot_month.columns)
+        # Adding one-hot columns
+        df = df.join(one_hot_month)
+        # Dropping month_year
+        df = df.drop(columns=["month_year"])
+
+        return df, feats
 
     def add_y_24h(self, df):
         """
@@ -67,7 +120,7 @@ class Preprocessor:
         """
         one_day_timesteps = (1 * 24 * 60) // 5
         df["y_24h"] = df["y"].shift(one_day_timesteps)
-        return df
+        return df, ["y_24h"]
 
     def add_y_yesterday(self, df):
         """
@@ -85,28 +138,50 @@ class Preprocessor:
         )
         # Renaming columns
         df = df.rename(columns={"y_x": "y", "y_y": "y_yesterday"})
-        return df
+        return df, ["y_yesterday"]
 
     def add_y_prev(self, df):
         """
         Adding the previous recorded y (5-minutes ago)
         """
         df["y_prev"] = df["y"].shift(1)
-        return df
+        return df, ["y_prev"]
 
     def add_features(self, df):
         """
         Adding features
         """
-        df = self.add_time_of_hour(df)
-        df = self.add_time_of_day(df)
-        df = self.add_time_of_week(df)
-        df = self.add_time_of_year(df)
-        df = self.add_y_24h(df)
-        # df = self.add_y_yesterday(df)
+        new_feat = []
+
+        # df, feat_list = self.add_time_of_hour(df)
+        # new_feat += feat_list
+
+        df, feat_list = self.add_time_of_day_min(df)
+        new_feat += feat_list
+
+        # df, feat_list = self.add_time_of_week_sin_cos(df)
+        # new_feat += feat_list
+
+        df, feat_list = self.add_time_of_week_one_hot(df)
+        new_feat += feat_list
+
+        # df, feat_list = self.add_time_of_year_sin_cos(df)
+        # new_feat += feat_list
+
+        df, feat_list = self.add_time_of_year_one_hot(df)
+        new_feat += feat_list
+
+        df, feat_list = self.add_y_24h(df)
+        new_feat += feat_list
+
+        # df, feat_list = self.add_y_yesterday(df)
+        # new_feat += feat_list
+
         # Adding prev_y last so its always the last column
-        df = self.add_y_prev(df)
-        return df
+        df, feat_list = self.add_y_prev(df)
+        new_feat += feat_list
+
+        return df, new_feat
 
     def preprocessing_df(self, df, train_df):
         """
@@ -117,34 +192,34 @@ class Preprocessor:
         clamped = winsorize(y, limits=[0.005, 0.005])
         df["y"] = np.array(clamped)
 
-        # Normalizing using a scaler
-        scale_features = df[[
+        # Declaring original features that are to be scaled
+        original_feat = [
             "hydro", "micro", "thermal", "wind", "river", "total", "y",
             "sys_reg", "flow"
-        ]]
+        ]
+        # Normalizing using a scaler
+        scale_features = df[original_feat]
         if train_df:
-            df[[
-                "hydro", "micro", "thermal", "wind", "river", "total", "y",
-                "sys_reg", "flow"
-            ]] = self.min_max_scaler.fit_transform(scale_features)
+            df[original_feat] = self.min_max_scaler.fit_transform(
+                scale_features)
         else:
-            df[[
-                "hydro", "micro", "thermal", "wind", "river", "total", "y",
-                "sys_reg", "flow"
-            ]] = self.min_max_scaler.transform(scale_features)
+            df[original_feat] = self.min_max_scaler.transform(scale_features)
 
         # Adding features after scaling
-        df = self.add_features(df)
+        df, new_feat = self.add_features(df)
+
+        X_feat = original_feat + new_feat
+        X_feat.remove("y")
 
         # Removing rows that contain NaN
         df = df.dropna()
 
-        return df
+        return df, X_feat
 
     def preprocess(self):
-        train_df = self.preprocessing_df(self.train_df, train_df=True)
-        val_df = self.preprocessing_df(self.val_df, train_df=False)
-        return train_df, val_df
+        train_df, X_feat = self.preprocessing_df(self.train_df, train_df=True)
+        val_df, _ = self.preprocessing_df(self.val_df, train_df=False)
+        return train_df, val_df, X_feat
 
     def df_to_x(self, df, seq_len):
         np_df = df.to_numpy()
@@ -163,4 +238,4 @@ class Preprocessor:
 if __name__ == "__main__":
     pp = Preprocessor(train_path="datasets/no1_train.csv",
                       val_path="datasets/no1_validation.csv")
-    train_df, val_df = pp.preprocess()
+    train_df, val_df, X_feat = pp.preprocess()
